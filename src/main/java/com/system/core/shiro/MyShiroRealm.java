a927889b2810df702c97f668e8b9b2a1287a3b5c
huangxiaoshan <huangxs@quantdo.com.cn>
2018-08-17 18:50:21 +0800

    temp

--- src/main/java/com/system/core/shiro/MyShiroRealm.java
+++ src/main/java/com/system/core/shiro/MyShiroRealm.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("MyShiroRealm")
 public class MyShiroRealm extends AuthorizingRealm {
 
     @Override
